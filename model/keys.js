f6bf90a575c8b563efd22cb3e7c2657da5aa10e8
lakchchayam khare <lakchchayam.khare@gmail.com>
2020-11-15 15:45:29 +0530

    Sheryishkill Version 1.0.0

---
+++ model/keys.js
@@ -0,0 +1,4 @@
+exports.url = {
+    mongodb: 'mongodb+srv://dhanesh-malviya:dhanesh123@mastercluster.i7cpa.mongodb.net/major01r3?retryWrites=true&w=majority',
+    jwtSecretKey: 'JWT Major Project Ket M01R3@'
+}
